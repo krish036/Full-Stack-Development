55a421cbe952858b947347d55e8be026ecec66f3
krish036 <90232766+krish036@users.noreply.github.com>
2022-09-19 11:15:10 +0530

    commit

---
+++ Practical-5/src/App.js
@@ -0,0 +1,28 @@
+// inside src/App.js
+// Replace previous code with this.
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+
+function App() {
+  const {register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  
+  return (
+    <>
+      <p className="title">Registration Form</p>
+
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("name")} />
+        <input type="email" {...register("email", { required: true })} />
+        {errors.email && <span style={{ color: "red" }}>
+        *Email* is mandatory </span>}
+        <input type="password" {...register("password")} />
+        <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+      </form>
+    </>
+  );
+}
+export default App;
---
+++ Practical-5b/src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router,  Routes, Route } from "react-router-dom";
+import { AboutUs, OurAim, OurVision } from "./pages/AboutUs";
+import {
+  Services,
+  ServicesOne,
+  ServicesTwo,
+  ServicesThree,
+} from "./pages/Services";
+import { Events, EventsOne, EventsTwo } from "./pages/Events";
+import Contact from "./pages/ContactUs";
+import Support from "./pages/Support";
+function App() {
+  return (
+    <Router>
+      <Sidebar />
+      < Routes>
+        <Route path="/about-us" exact element={AboutUs} />
+        <Route path="/about-us/aim" exact element={OurAim} />
+        <Route path="/about-us/vision" exact element={OurVision} />
+        <Route path="/services" exact element={Services} />
+        <Route path="/services/services1" exact element={ServicesOne} />
+        <Route path="/services/services2" exact element={ServicesTwo} />
+        <Route path="/services/services3" exact element={ServicesThree} />
+        <Route path="/contact" exact element={Contact} />
+        <Route path="/events" exact element={Events} />
+        <Route path="/events/events1" exact element={EventsOne} />
+        <Route path="/events/events2" exact element={EventsTwo} />
+        <Route path="/support" exact element={Support} />
+      </ Routes>
+    </Router>
+  );
+}
+  
+export default App;
